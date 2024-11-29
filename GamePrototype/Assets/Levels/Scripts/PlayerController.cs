f03eef35afb0a230928537f0c745518f0a9dce24
JacobGreathouse <63613279+JacobGreathouse@users.noreply.github.com>
2024-11-28 18:59:11 -0700

    First publish

---
+++ GamePrototype/Assets/Levels/Scripts/PlayerController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("----- Components -----")]
+    [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreMask;
+
+    [Header("----- Stats -----")]
+    [SerializeField] [Range(1, 5)]int speed;
+    [SerializeField] [Range(2, 4)]int sprintMod;
+    [SerializeField] [Range(1, 3)]int jumpMax;
+    [SerializeField] [Range(5, 20)]int jumpSpeed;
+    [SerializeField] [Range(15, 40)]int gravity;
+
+    [Header("----- Gun Stats -----")]
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDistance;
+    [SerializeField] float shootRate;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    int jumpCount;
+
+    bool isShooting;
+    bool isSprinting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDistance, Color.red);
+
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        if(controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+
+        // moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // transform.position += moveDir * speed * Time.deltaTime
+
+        moveDir = (transform.right * Input.GetAxis("Horizontal")) +
+                  (transform.forward * Input.GetAxis("Vertical"));
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+
+        controller.Move(playerVel * Time.deltaTime);
+        playerVel.y -= gravity * Time.deltaTime;
+
+        if((controller.collisionFlags & CollisionFlags.Above) != 0)
+        {
+            playerVel = Vector3.zero;
+        }
+
+        if(Input.GetButton("Fire1") && !isShooting)
+        {
+            StartCoroutine(shoot());
+        }
+    }
+
+    void jump()
+    {
+        if(Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+            
+        }
+    }
+
+    void sprint()
+    {
+        if(Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+            isSprinting = true;
+        }
+        else if(Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+            isSprinting = false;
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        // shoot code goes
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDistance, ~ignoreMask))
+        {
+            Debug.Log(hit.collider.name);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            if(dmg != null)
+            {
+                dmg.takeDamage(shootDamage);
+            }
+        }
+
+
+        yield return new WaitForSeconds(shootRate);
+
+        isShooting = false;
+    }
+}
