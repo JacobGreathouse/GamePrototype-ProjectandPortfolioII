bf6f0a8f34899ea01824221635be5fb6a1c491c3
Windwarriorplays <windwarrior14430@gmail.com>
2024-12-06 20:53:38 -0600

    Fireball and Arrow
    finished coding the damage and added a fireball and Arrow will commit again when done with lava pit and lightning

--- GamePrototype/Assets/Levels/Scripts/Damage.cs
+++ GamePrototype/Assets/Levels/Scripts/Damage.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+
+    enum damageType { moving, stationary}
+    [SerializeField] damageType type;
+    [SerializeField] Rigidbody rb;
+
     [SerializeField] int damageAmount;
+    [SerializeField] float speed;
+    [SerializeField] int destroyTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       if(type == damageType.moving)
+        {
+            rb.velocity = transform.forward * speed;
+            Destroy(gameObject, destroyTime);
+        }
     }
     //just type OnTriggerEnter to access what you need for
     //an effect to happen when you enter the trigger area
@@ -20,6 +33,11 @@ public class Damage : MonoBehaviour
         if(dmg != null )
         {
             dmg.takeDamage(damageAmount);
+        }
+
+        if(type == damageType.moving)
+        {
+            Destroy(gameObject);
         }
      
     }
