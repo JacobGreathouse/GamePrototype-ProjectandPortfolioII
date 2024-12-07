695ccdabb743a37fcbf4c5c0152f41eb4eff32b7
Windwarriorplays <windwarrior14430@gmail.com>
2024-12-07 10:16:33 -0600

    in lecture changes

--- GamePrototype/Assets/Levels/Scripts/Damage.cs
+++ GamePrototype/Assets/Levels/Scripts/Damage.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class Damage : MonoBehaviour
@@ -13,6 +12,8 @@ public class Damage : MonoBehaviour
     [SerializeField] int damageAmount;
     [SerializeField] float speed;
     [SerializeField] int destroyTime;
+
+    bool hasDamaged;
 
     // Start is called before the first frame update
     void Start()
