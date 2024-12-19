acebf45405630c678a0e29c74a6f2df5a7c9aa5b
Windwarriorplays <windwarrior14430@gmail.com>
2024-12-19 06:33:08 -0600

    finishing touches
     tourches down mob spawner hidden rave tourches done and doors are added to the mini map and ceiling is done

---
+++ GamePrototype/Assets/Levels/Scripts/ColorChange.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    [SerializeField] Light rave;
+    private float lerp = 20f;
+
+    void Start()
+    {
+        StartCoroutine(ColorChangeRoutine());
+    }
+
+    private IEnumerator ColorChangeRoutine()
+    {
+        while (true)
+        {
+            var startColor = rave.color;
+            var endColor = new Color32(System.Convert.ToByte(Random.Range(0, 255)), System.Convert.ToByte(Random.Range(0, 255)), System.Convert.ToByte(Random.Range(0, 255)), 255);
+
+            float t = 0;
+            while (t < 1)
+            {
+                t += lerp + Time.deltaTime; // Multiply Time.deltaTime by some constant to speed/slow the transition.
+                rave.color = Color.Lerp(startColor, endColor, t);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(1); ;
+        }
+    }
+}
