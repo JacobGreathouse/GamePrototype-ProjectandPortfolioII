cf10ea0017a779538a392b6487542ec53113da52
Gabe Gillette <92653198+gabeGillette@users.noreply.github.com>
2025-01-15 12:02:03 -0500

    Portal scripting

--- GamePrototype/Assets/Levels/Scripts/Portal.cs
+++ GamePrototype/Assets/Levels/Scripts/Portal.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    enum PORTALTYPE {SCENE_LOCAL, SCENE_TRANSITION}
+
+
+    [Header("~~~ Global ~~~")]
+    [SerializeField] PORTALTYPE _type;
     [SerializeField] Camera _exitCam;
+    //[SerializeField] Collider _renderTrigger;
+    [SerializeField] Collider _teleportTrigger;
+
+    [Header("~~~ Scene Local ~~~")]
+    [SerializeField] GameObject _exitNode;
+
+    [Header("~~~ Scene Transition ~~~")]
+    [SerializeField] [Range(0, 1000)] int _sceneIndex;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _exitCam.transform.SetPositionAndRotation(_exitNode.transform.position, _exitNode.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _exitCam.transform.position = Camera.main.transform.position;
-        _exitCam.transform.rotation = gamemanager.instance.transform.rotation;
+        //_exitCam.transform.position = Camera.main.transform.position;
+        //_exitCam.transform.rotation = gamemanager.instance.transform.rotation;
     }
 }
