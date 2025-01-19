6ecc69be5688460f2979b64ec0903356ed3b2746
JacobGreathouse <63613279+JacobGreathouse@users.noreply.github.com>
2025-01-18 18:25:19 -0700

    Set up basic shop with functionality to buy Health potion

---
+++ GamePrototype/Assets/Levels/Scripts/ShopScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+
+    bool playerInTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInTrigger)
+        {
+            if(Input.GetButtonDown("Interact"))
+            {
+                BuyHealthPotion();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+            return;
+
+        playerInTrigger = true;
+        
+    }
+
+    void BuyHealthPotion()
+    {
+        gamemanager.instance.player.GetComponent<PlayerController>().setHealthPotion(1);
+    }
+}
